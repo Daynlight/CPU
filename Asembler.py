98e72080fe2b2c6d1c4fb15e125b6bdb5652d6c3
daynlight <danielstodulski000@gmail.com>
2022-12-03 14:58:05 +0100

    Programs

--- Asembler.py
+++ Asembler.py
@@ -11,7 +11,7 @@ b=1
 endline=8
 jmpa=0
 for L in L:
-    if(L[0:3]=="pri"): # 2 op przez każdą iterę - Wypisz ciąg znaków do w "wartość"
+    if(L[0:3]=="pri"): 
         i=5
         while(L[i]!='"'):
             c=["e",0,str(hex(ord(L[i]))[2:5]),6,0,0]
@@ -30,7 +30,7 @@ for L in L:
             f.write(w)
             w=""
             f.close
-    if(L[0:3]=="nel"): # 2 op - Go to next line
+    if(L[0:3]=="nel"): 
         c=["e",0,"d",6,0,0]
         ia=0
         w=""
@@ -46,7 +46,7 @@ for L in L:
         f.write(w)
         w=""
         f.close   
-    if(L[0:3]=="var"): # 1 op - Ustaw (rejsetr) na "wartość"
+    if(L[0:3]=="var"): 
         i=9
         r=L[5]
         d=""
@@ -68,7 +68,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="prr"): # 1 op - Z (rejestru) do (rejestru)
+    if(L[0:3]=="prr"): 
         ia=0
         r=L[5]
         d=L[9]
@@ -85,7 +85,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="out"): # 1 op - wypisz z (rejestru)
+    if(L[0:3]=="out"): 
         ia=0
         r=L[5]
         c=["6",r,0]
@@ -101,7 +101,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="get"): # 1 op - Get from keybord do (Rejestru)
+    if(L[0:3]=="get"): 
         ia=0
         r=L[5]
         c=["5",r,0]
@@ -117,7 +117,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="sme"): # 1 op - Set data from (Register) to memory location = R4
+    if(L[0:3]=="sme"): 
         ia=0
         r=L[5]
         c=["1",r,0]
@@ -133,7 +133,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="gme"): # 1 op - Get from memory to (Register) location = R4
+    if(L[0:3]=="gme"): 
         ia=0
         r=L[5]
         c=["2",r,0]
@@ -149,7 +149,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="jmp"): # 1 op - jump to (z rejestru) need 1 from compare and disk = R3
+    if(L[0:3]=="jmp"): 
         ia=0
         r=L[5]
         c=["3",r,0]
@@ -165,7 +165,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="alu"): # 1 op - AlU Op = R0 A = R1 B = R2 Wynik = R3
+    if(L[0:3]=="alu"): 
         ia=0
         c=["4",0,0]
         jmpa=jmpa+3
@@ -180,7 +180,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="com"): # 1 op - Compar R1 and R2 if in R3 | 0 > , 1 = , 2 <, 3 <=, 4 !=, 5 >=, 6 True, 7 False
+    if(L[0:3]=="com"): 
         ia=0
         c=["b",0,0]
         jmpa=jmpa+3
@@ -195,7 +195,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="wai"): # 1 op - wait to input
+    if(L[0:3]=="wai"): 
         ia=0
         c=["a",0,0]
         jmpa=jmpa+3
@@ -210,7 +210,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="dsc"): # 1 op - jmp to disck from data in (Register)
+    if(L[0:3]=="dsc"): 
         ia=0
         r=L[5]
         c=["d",r,0]
@@ -226,9 +226,9 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="sav"): # only 1 bit - Save on DISK R3 = DISK R2 = DATA R1 = Location
+    if(L[0:3]=="sav"): 
         ia=0
-        c=["8"]
+        c=[8,0,0]
         jmpa=jmpa+1
         while(ia<len(c)):
             w=w+ str(c[ia])+" "
@@ -241,7 +241,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="dat"): # only 1 bit - Save on DISK R3 = DISK R2 = DATA R1 = Location
+    if(L[0:3]=="dat"): 
         ia=0
         r=L[5]
         a=L[9]
@@ -258,7 +258,7 @@ for L in L:
         f.write(w)
         w=""
         f.close
-    if(L[0:3]=="end"): # only 1 bit - Save on DISK R3 = DISK R2 = DATA R1 = Location
+    if(L[0:3]=="end"): 
         ia=0
 
         c=["f","0",""]
@@ -277,4 +277,3 @@ for L in L:
 
 print("Wielkość danych = "+str(jmpa))
 
-input("END")
